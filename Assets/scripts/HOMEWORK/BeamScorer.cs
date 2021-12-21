bc1599477f466172121455d7888473be4915e9bd
mikel <mikelk96@gmail.com>
2021-12-21 13:23:51 +0200

    beams check for player

--- Assets/scripts/HOMEWORK/BeamScorer.cs
+++ Assets/scripts/HOMEWORK/BeamScorer.cs
@@ -6,17 +6,22 @@ using Com.MyCompany.MyGame;
 
 public class BeamScorer : MonoBehaviour
 {
-
+    [SerializeField]
+    string m_triggerTag;
     public PlayerManager playerManager;
     public int scoreOnHit = 1;
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.tag != m_triggerTag)
+            return;
         playerManager.score += scoreOnHit; 
     }
 
     private void OnTriggerStay(Collider other)
     {
+        if(other.tag != m_triggerTag)
+            return;
         playerManager.score += scoreOnHit; 
 
     }
